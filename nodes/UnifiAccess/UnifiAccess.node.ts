4f3ca57c6ff4dfc889100e1d9e77cbd2041f8b07
Rick Yorgason <rick@firefang.com>
2026-02-05 18:39:53 -0700

    Fixed delete NFC card getting called on assign NFC.

--- nodes/UnifiAccess/UnifiAccess.node.ts
+++ nodes/UnifiAccess/UnifiAccess.node.ts
@@ -262,7 +262,7 @@ export class UnifiAccess implements INodeType {
           }
 
           // 3.8 Unassign an NFC Card from a User
-          if (operation === 'assignNfc') {
+          if (operation === 'delete') {
             const userId = this.getNodeParameter('userId', i) as string;
             const token = this.getNodeParameter('token', i) as string;
 
